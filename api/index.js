3d3fda2a563c20f69fefffa81c90cd567f16c701
Nguyen Xuan Nam <e0969384@u.nus.edu>
2023-05-29 06:19:44 +0800

    add backend express + prisma

---
+++ api/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const prisma = require("./db/prisma");
+dotenv.config();
+
+const app = express();
+
+const port = 3001;
+
+app.get('/', async (req,res) => {
+  })
+    const users = await prisma.user.findMany()
+    const names = users.map((user) => user.name)
+    res.send(`There are ${names.length} which are ${names.join(", ")}`);
+
+app.listen(port, () => console.log(`App listen at port ${port}!`));
---
+++ api/prisma/migrations/20230528214122_authentication_with_optional/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "studentID" DROP NOT NULL,
+ALTER COLUMN "faculty" DROP NOT NULL;
