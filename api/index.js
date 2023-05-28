cace5f9f8fe7dca592dcb466109d015bc912cdd9
Nguyen Xuan Nam <e0969384@u.nus.edu>
2023-05-29 06:23:22 +0800

    fixing wrong syntax

--- api/index.js
+++ api/index.js
@@ -8,9 +8,9 @@ const app = express();
 const port = 3001;
 
 app.get('/', async (req,res) => {
-  })
+  
     const users = await prisma.user.findMany()
     const names = users.map((user) => user.name)
     res.send(`There are ${names.length} which are ${names.join(", ")}`);
-
+})
 app.listen(port, () => console.log(`App listen at port ${port}!`));
