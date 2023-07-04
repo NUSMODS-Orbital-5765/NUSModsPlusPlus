6f1f1f43c3baef28296e18e290dd5dd655fb4a48
mochicooki3 <132681043+mochicooki3@users.noreply.github.com>
2023-07-05 01:18:33 +0800

    included part of welcome messages

--- client/src/SignUp/AdminSignUpPage.js
+++ client/src/SignUp/AdminSignUpPage.js
@@ -10,6 +10,35 @@ import {
 import { LogoComponent } from "../StyledComponents";
 import { CarouselComponent } from "../StyledComponents";
 
+// list of welcome messages that we intend to map
+export const WelcomeMessageCarousel = [
+  <div>
+    <Box sx={{ margin: "20px" }}>
+      <Typography sx={{ color: "#004d80", fontSize: "50px", fontWeight: 700 }}>
+        Plan well, score well.
+      </Typography>
+      <Typography
+        sx={{ fontSize: "20px", marginTop: "20px", color: "#004d80" }}
+      >
+        Join the community today.
+      </Typography>
+    </Box>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "flex-end",
+      }}
+    >
+      <img
+        style={{ width: "60%" }}
+        alt="Sample Icon"
+        src="/join-community-icon.png"
+      />
+    </Box>
+  </div>,
+];
+
 // admin sign up component
 const AdminSignUpPage = () => {
   return (
@@ -23,37 +52,43 @@ const AdminSignUpPage = () => {
     >
       <Box
         sx={{
+          width: "50%",
+          height: "100vh",
           backgroundColor: "#e7f2ff",
-          marginRight: "30px",
-          alignItems: "center",
-          justifyItems: "center",
           display: "flex",
-          flexDirection: "row",
+          flexDirection: "column",
+          justifyContent: "flex-end",
+          alignItems: "flex-start",
         }}
       >
-        <Box>
+        <Box sx={{ margin: "20px" }}>
           <Typography
-            sx={{
-              margin: "20px",
-              color: "#004d80",
-              fontSize: "40px",
-              fontWeight: 700,
-            }}
+            sx={{ color: "#004d80", fontSize: "50px", fontWeight: 700 }}
           >
             Plan well, score well.
           </Typography>
-          <Typography sx={{ marginTop: "20px", color: "#004d80" }}>
+          <Typography
+            sx={{ fontSize: "20px", marginTop: "20px", color: "#004d80" }}
+          >
             Join the community today.
           </Typography>
         </Box>
-        <img
-          sx={{ width: "1%" }}
-          alt="Sample Icon"
-          src="/join-community-icon.png"
-        />
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "flex-end",
+          }}
+        >
+          <img
+            style={{ width: "60%" }}
+            alt="Sample Icon"
+            src="/join-community-icon.png"
+          />
+        </Box>
       </Box>
 
-      <Box>
+      <Box sx={{ width: "50%" }}>
         <LogoComponent />
         <Card sx={{ borderRadius: "10px", marginTop: "20px" }}>
           <CardContent sx={{ margin: "20px" }}>
