a4aef3ef4e5a84b8fe8c24b3431870024d281e14
mochicooki3 <132681043+mochicooki3@users.noreply.github.com>
2023-07-10 15:29:59 +0800

    title for modules page

--- client/src/ModulesPage.js
+++ client/src/ModulesPage.js
@@ -1,11 +1,37 @@
 import AppBarComponent from "./AppBar/AppBarComponent";
 import DrawerComponent from "./DrawerComponent";
+import { Box, Typography, Card, CardContent } from "@mui/material";
 
 const ModulesPage = () => {
   return (
     <div className="homepage">
       <DrawerComponent defaultTab={3} />
       <AppBarComponent />
+      <Box className="remainingViewport">
+        <Box
+          sx={{
+            margin: "55px",
+            marginTop: "20px",
+            borderRadius: "10px",
+            backgroundColor: "#e7f2ff",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Typography
+            sx={{
+              margin: "30px",
+              fontSize: "40px",
+              fontWeight: "700",
+              color: "#004d80",
+            }}
+          >
+            Plan your semesters efficiently and effectively.
+          </Typography>
+        </Box>
+      </Box>
     </div>
   );
 };
