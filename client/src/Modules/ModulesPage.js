5f46500d11c8f2d90805a77c55428629987ba420
mochicooki3 <132681043+mochicooki3@users.noreply.github.com>
2023-07-17 16:09:52 +0800

    update to ensure plan is only created if the academic info is valid

--- client/src/Modules/ModulesPage.js
+++ client/src/Modules/ModulesPage.js
@@ -3,30 +3,18 @@ import DrawerComponent from "../DrawerComponent";
 import {
   Box,
   Typography,
-  Button,
   Tabs,
   Tab,
-  Dialog,
-  DialogContent,
-  FormControl,
-  InputLabel,
-  MenuItem,
-  Select,
   IconButton,
   Snackbar,
   Alert,
 } from "@mui/material";
 import AddRoundedIcon from "@mui/icons-material/AddRounded";
-import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
-import React, { useState, useEffect } from "react";
+
+import React, { useState } from "react";
 import ModulesDisplay from "./ModulesDisplay";
-import { SlideUpTransition } from "../StyledComponents";
-import {
-  FormAutocomplete,
-  FormFacultyMajorField,
-  FormMinorField,
-} from "../FormStyledComponents";
-import { majorList, progsList } from "../Constants";
+
+import CreatePlanDialog from "./CreatePlanDialog";
 
 // header for modules page
 export const ModulesPageHeader = () => {
@@ -83,156 +71,6 @@ export const emptyAcademicInfo = {
   programme: "",
 };
 
-// dialog for creating a new plan
-export const CreatePlanDialog = ({
-  openDialog,
-  handleCloseDialog,
-  currentAcademicInfo,
-  onSubmit,
-}) => {
-  const [academicPlanInfo, setAcademicPlanInfo] = useState(currentAcademicInfo);
-
-  const handleAcademicPlanInfo = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
-    setAcademicPlanInfo({ ...academicPlanInfo, [name]: value });
-  };
-
-  const [academicPlan, setAcademicPlan] = useState("");
-  const handleAcademicPlanChange = (event) => {
-    setAcademicPlan(event.target.value);
-  };
-
-  const handleMinorInfo = (value) => {
-    setAcademicPlanInfo({
-      ...academicPlanInfo,
-      ["minor"]: value,
-    });
-  };
-
-  const submitAcademicPlanInfo = () => {
-    handleCloseDialog();
-    onSubmit(academicPlanInfo);
-    console.log(academicPlanInfo);
-  };
-
-  // check form completion status
-  const [isFormComplete, setIsFormComplete] = useState(false);
-
-  const handleFormCompletion = (fieldErrors) => {
-    const isComplete = Object.values(fieldErrors).every((error) => !error);
-    setIsFormComplete(isComplete);
-  };
-
-  const fieldErrors = {
-    faculty: academicPlanInfo.faculty === "",
-    primaryDegree: academicPlanInfo.primaryDegree === "",
-  };
-
-  useEffect(() => {
-    handleFormCompletion(fieldErrors);
-  }, [academicPlanInfo]);
-
-  return (
-    <Dialog
-      open={openDialog}
-      TransitionComponent={SlideUpTransition}
-      fullWidth
-      maxWidth="md"
-      borderRadius="10px"
-    >
-      <DialogContent sx={{ margin: "10px" }}>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            justifyItems: "center",
-            justifyContent: "space-between",
-          }}
-        >
-          <Typography sx={{ fontSize: "35px", fontWeight: 700 }}>
-            Create New Plan
-          </Typography>
-          <IconButton onClick={handleCloseDialog}>
-            <CloseRoundedIcon sx={{ fontSize: "30px" }} color="error" />
-          </IconButton>
-        </Box>
-        <Box
-          sx={{ marginTop: "20px", display: "flex", flexDirection: "column" }}
-        >
-          <Box sx={{ marginBottom: "20px" }}>
-            <FormFacultyMajorField
-              filledMajor=""
-              filledFaculty=""
-              setfn={handleAcademicPlanInfo}
-            />
-          </Box>
-          <Box sx={{ marginBottom: "20px" }}>
-            <FormControl fullWidth>
-              <InputLabel id="academic-plan">Academic Plan</InputLabel>
-              <Select label="Academic Plan" onChange={handleAcademicPlanChange}>
-                <MenuItem value={"Single Degree"}>Single Degree</MenuItem>
-                <MenuItem value={"Double Degree"}>Double Degree</MenuItem>
-                <MenuItem value={"Double Major"}>Double Major</MenuItem>
-              </Select>
-            </FormControl>
-          </Box>
-          {academicPlan === "Double Degree" && (
-            <Box sx={{ marginBottom: "20px" }}>
-              <FormAutocomplete
-                optionsList={majorList}
-                label="Second Degree"
-                name="secondDegree"
-                setfn={handleAcademicPlanInfo}
-              />
-            </Box>
-          )}
-          {academicPlan === "Double Major" && (
-            <Box sx={{ marginBottom: "20px" }}>
-              <FormAutocomplete
-                optionsList={majorList}
-                label="Second Major"
-                name="secondMajor"
-                setfn={handleAcademicPlanInfo}
-              />
-            </Box>
-          )}
-          <Box sx={{ marginBottom: "20px" }}>
-            <FormMinorField setfn={handleMinorInfo} />
-          </Box>
-          <Box sx={{ marginBottom: "20px" }}>
-            <FormAutocomplete
-              optionsList={progsList}
-              label="Special Programme (if any)"
-              name="programme"
-              setfn={handleAcademicPlanInfo}
-            />
-          </Box>
-        </Box>
-        <Box
-          sx={{
-            marginTop: "10px",
-            marginBottom: "10px",
-            display: "flex",
-            justifyContent: "flex-end",
-            alignItems: "flex-end",
-          }}
-        >
-          <Button
-            variant="contained"
-            color="primary"
-            disabled={!isFormComplete}
-            onClick={submitAcademicPlanInfo}
-          >
-            Create
-          </Button>
-        </Box>
-      </DialogContent>
-    </Dialog>
-  );
-};
-
 // main page component
 const ModulesPage = () => {
   // information about the new tab
@@ -262,7 +100,7 @@ const ModulesPage = () => {
       console.log(newTabAcademicInfo);
       newTabAcademicInfo !== emptyAcademicInfo &&
         setTabsList([...tabsList, "Draft Plan 1"]);
-      console.log(newTabAcademicInfo);
+      setNewTabAcademicInfo(emptyAcademicInfo);
     } else if (tabsList.length === 4) {
       setPlanAlert(true);
     } else {
@@ -270,6 +108,7 @@ const ModulesPage = () => {
       const newDraftPlan = "Draft Plan " + tabsList.length.toString();
       newTabAcademicInfo !== emptyAcademicInfo &&
         setTabsList([...tabsList, newDraftPlan]);
+      setNewTabAcademicInfo(emptyAcademicInfo);
     }
   };
 
