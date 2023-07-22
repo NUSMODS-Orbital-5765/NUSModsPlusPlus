071c2fe386bd660fbf8886e264152ef089a9ee13
vnnamng <e0969384@u.nus.edu>
2023-07-22 11:15:26 +0800

    fix the function of get3k4k

--- client/src/libs/get3k4kModuleList.js
+++ client/src/libs/get3k4kModuleList.js
@@ -3,13 +3,13 @@ import axios from 'axios';
 export default async function get3k4kModuleList (prefixList, kLevel) {
     try{
     const APILINK = `${process.env.REACT_APP_API_LINK}/module/get-requirement`;
-    const moduleList = await axios.post(APILINK, 
+    const returnRequest = await axios.post(APILINK, 
         {
             prefixList: prefixList,
             kLevel: kLevel
         }
     )
-    return moduleList
+    return returnRequest.data.result
     }
     catch(err) {
         console.log(err);
