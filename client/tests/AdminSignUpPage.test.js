b1f9037eb2d1517e69e9381673a3d950daa916c5
mochicooki3 <132681043+mochicooki3@users.noreply.github.com>
2023-07-23 15:01:44 +0800

    updated admin sign up page testing

--- client/tests/AdminSignUpPage.test.js
+++ client/tests/AdminSignUpPage.test.js
@@ -3,6 +3,8 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import AdminSignUpPage from "../src/SignUp/AdminSignUpPage";
 import { MemoryRouter } from "react-router-dom";
+import axios from "axios";
+import axiosMock from "axios-mock-adapter";
 
 beforeEach(() => {
   render(
@@ -20,15 +22,22 @@ describe("AdminSignUpPage", () => {
     ).toBeInTheDocument();
   });
 
+  // testing logo render
+  test("renders the component", () => {
+    expect(
+      screen.getByText("Welcome! Let's get you settled.")
+    ).toBeInTheDocument();
+  });
+
   // testing form fields update
   test("updates name input field correctly", () => {
-    const nameInput = screen.getByLabelText("Name");
+    const nameInput = screen.getByLabelText("Name *");
     fireEvent.change(nameInput, { target: { value: "John Doe" } });
     expect(nameInput).toHaveValue("John Doe");
   });
 
   test("updates staff ID input field correctly", () => {
-    const staffIdInput = screen.getByLabelText("Staff ID");
+    const staffIdInput = screen.getByLabelText("Staff ID *");
     fireEvent.change(staffIdInput, { target: { value: "123456" } });
     expect(staffIdInput).toHaveValue("123456");
   });
